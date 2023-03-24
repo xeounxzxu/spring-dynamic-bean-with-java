e43ac24ca5b046b43242deb177d339c0ec09a7e6
jgspark <pnw123456@icloud.com>
2023-03-24 23:00:26 +0900

    fix: Read Map BeanFactory Bug Fixed It
    - Map Bean Not Wokring Bean Fixed

--- src/main/java/com/example/dynamicbeans/service/factory/UserServiceMapFactory.java
+++ src/main/java/com/example/dynamicbeans/service/factory/UserServiceMapFactory.java
@@ -1,28 +1,20 @@
 package com.example.dynamicbeans.service.factory;
 
 import com.example.dynamicbeans.service.UserCreatedService;
-import org.apache.commons.text.CaseUtils;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 @Service
 public class UserServiceMapFactory {
 
     private final Map<String, UserCreatedService> stringUserCreatedServiceMap;
 
-    public UserServiceMapFactory(List<UserCreatedService> services) {
-        this.stringUserCreatedServiceMap = services.stream()
-                .collect(Collectors.toMap((c) ->
-                                //todo : fix it
-                                c.getClass().getSimpleName().su
-                        , Function.identity()));
+    public UserServiceMapFactory(Map<String, UserCreatedService> stringUserCreatedServiceMap) {
+        this.stringUserCreatedServiceMap = stringUserCreatedServiceMap;
     }
 
     public String created(UserCreatedService.Name name) {
-        return stringUserCreatedServiceMap.get(name).created();
+        return stringUserCreatedServiceMap.get(name.getValue()).created();
     }
 }
