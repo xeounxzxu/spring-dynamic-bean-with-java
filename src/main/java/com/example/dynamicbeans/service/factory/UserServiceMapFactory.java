94ee64e111e55a846043d56852d9fbafaffdc353
jgspark <pnw123456@icloud.com>
2023-03-24 23:03:46 +0900

    refactor: Bean DI Changed
    - Bean DI Chnage

--- src/main/java/com/example/dynamicbeans/service/factory/UserServiceMapFactory.java
+++ src/main/java/com/example/dynamicbeans/service/factory/UserServiceMapFactory.java
@@ -1,18 +1,16 @@
 package com.example.dynamicbeans.service.factory;
 
 import com.example.dynamicbeans.service.UserCreatedService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceMapFactory {
 
     private final Map<String, UserCreatedService> stringUserCreatedServiceMap;
-
-    public UserServiceMapFactory(Map<String, UserCreatedService> stringUserCreatedServiceMap) {
-        this.stringUserCreatedServiceMap = stringUserCreatedServiceMap;
-    }
 
     public String created(UserCreatedService.Name name) {
         return stringUserCreatedServiceMap.get(name.getValue()).created();
