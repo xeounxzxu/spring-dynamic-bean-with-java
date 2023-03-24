3e1592c334721187dd46958418e7a8d8cc217491
박정규 <bagjeong-gyu@bagmilims-MacBook-Pro.local>
2023-03-24 19:46:07 +0900

    first commit

---
+++ src/main/java/com/example/dynamicbeans/DynamicBeansApplication.java
@@ -0,0 +1,13 @@
+package com.example.dynamicbeans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicBeansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicBeansApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dynamicbeans/DynamicBeansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dynamicbeans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicBeansApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
